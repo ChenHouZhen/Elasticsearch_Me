654624a4862acdac3846cebc29efd3f7ec01c81f
chenhouzhen <chenhouzhen@hqjy.com>
2018-12-19 23:33:54 +0800

    first commit

---
+++ javahighlevelrestclient-elasticsearch/src/main/java/com/chenhz/javahighlevelrestclientelasticsearch/JavahighlevelrestclientElasticsearchApplication.java
@@ -0,0 +1,14 @@
+package com.chenhz.javahighlevelrestclientelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavahighlevelrestclientElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavahighlevelrestclientElasticsearchApplication.class, args);
+    }
+
+}
+
---
+++ transportclient-elasticsearch/src/main/java/com/chenhz/transportclientelasticsearch/TransportclientElasticsearchApplication.java
@@ -0,0 +1,14 @@
+package com.chenhz.transportclientelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportclientElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportclientElasticsearchApplication.class, args);
+    }
+
+}
+
