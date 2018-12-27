32cab5d38558df1b5e68a3aade85dba4980cc8f3
chenhouzhen <chenhouzhen@hqjy.com>
2018-12-27 17:40:54 +0800

    dd

--- transportclient-elasticsearch/src/main/java/com/chenhz/transportclientelasticsearch/controller/HelloController.java
+++ transportclient-elasticsearch/src/main/java/com/chenhz/transportclientelasticsearch/controller/HelloController.java
@@ -4,11 +4,13 @@ import com.chenhz.transportclientelasticsearch.config.EsConfigProps;
 import com.chenhz.transportclientelasticsearch.config.IndexConfigProps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.soap.Text;
 import java.util.Map;
 
 @RestController
@@ -28,4 +30,6 @@ public class HelloController {
         return configProps.toString() + "     " +
                 "" +indexConfigProps.toString();
     }
+
+
 }
